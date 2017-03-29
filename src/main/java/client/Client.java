55c263b36371c02fe1db2876208b55e48b116e04
Starrimus <starrimus7@gmail.com>
2017-03-29 09:42:41 +0300

    Added Maven

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,11 @@
+package main.java.client;
+
+/**
+ * Created by Starrimus on 29.03.2017.
+ */
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Client");
+    }
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,11 @@
+package main.java.server;
+
+/**
+ * Created by Starrimus on 29.03.2017.
+ */
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server");
+    }
+}
