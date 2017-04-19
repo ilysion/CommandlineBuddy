b9d31850d1f627fe40903f3102c77440a2ec3e26
DanBondarenko <danxfer@gmail.com>
2017-04-20 00:15:24 +0300

    Made several minor readability/elegance improvements.

--- src/main/java/server/Database.java
+++ src/main/java/server/Database.java
@@ -3,13 +3,17 @@ package server;
 import java.io.File;
 import java.sql.*;
 
-
 class Database {
     private static Database INSTANCE = null;
     private final static String DBFILE = "server.db";
 
     private Database() throws SQLException {
-        init();
+        File file = new File(DBFILE);
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DBFILE)) {
+            if (!file.exists()) {
+                connection.createStatement().executeUpdate("create table user (id integer PRIMARY KEY AUTOINCREMENT, name string, password string)");
+            }
+        }
     }
 
     static Database getDB() throws SQLException {
@@ -19,26 +23,12 @@ class Database {
         return INSTANCE;
     }
 
-    private void init() throws SQLException {
-        File file = new File(DBFILE);
-        boolean createTable = false;
-        if (!file.exists()) {
-            createTable = true;
-        }
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DBFILE);
-        if (createTable) {
-            connection.createStatement().executeUpdate("create table user (id integer PRIMARY KEY AUTOINCREMENT, name string, password string)");
-        }
-        connection.close();
-    }
-
     String getPassword(String username) throws SQLException {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DBFILE);
-             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM user WHERE name = ?");
-             ResultSet results = stmt.executeQuery();) {
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DBFILE)) {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM user WHERE name = ?");
+            ResultSet results = stmt.executeQuery();
             stmt.setString(1, username);
-            boolean exists = results.next();
-            if (exists) {
+            if (results.next()) {
                 return results.getString("password").replaceAll("\\s+", "");
             }
             else {
