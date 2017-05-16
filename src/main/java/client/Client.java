44298ec225d7e832271835ec4d49fee171b16354
ilysion <hannes.saariste@gmail.com>
2017-05-16 17:02:11 +0300

    ip validator

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -19,39 +19,45 @@ public class Client {
                 if (keyboard.hasNextLine()) {
                     String message = keyboard.nextLine();
                     if (message.startsWith("connect") && message.contains(":") && message.split("[ :]").length == 3) {
-                        // TODO ipv4 validator
-                        String[] address = message.split("[ :]");
-                        try (Socket socket = new Socket(address[1], Integer.parseInt(address[2]));
-                             DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
 
-                            System.out.println("Connected to " + address[1] + ":" + address[2]);
+                        if(ipValidator(message)){
 
-                            MessageReceiver messageReceiver = new MessageReceiver(socket);
-                            Thread msgThread = new Thread(messageReceiver);
+                            String[] address = message.split("[ :]");
+                            try (Socket socket = new Socket(address[1], Integer.parseInt(address[2]));
+                                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
 
-                            msgThread.start();
+                                System.out.println("Connected to " + address[1] + ":" + address[2]);
 
-                            while (!quit) {
-                                if (keyboard.hasNextLine()) {
-                                    message = keyboard.nextLine();
-                                    if (!socket.isClosed()) {
-                                        if (Objects.equals(message, ":quit")) {
-                                            System.out.println("Shutting down!");
-                                            msgThread.interrupt();
-                                            quit = true;
+                                MessageReceiver messageReceiver = new MessageReceiver(socket);
+                                Thread msgThread = new Thread(messageReceiver);
+
+                                msgThread.start();
+
+                                while (!quit) {
+                                    if (keyboard.hasNextLine()) {
+                                        message = keyboard.nextLine();
+                                        if (!socket.isClosed()) {
+                                            if (Objects.equals(message, ":quit")) {
+                                                System.out.println("Shutting down!");
+                                                msgThread.interrupt();
+                                                quit = true;
+                                            } else {
+                                                dos.writeUTF(message);
+                                            }
                                         } else {
-                                            dos.writeUTF(message);
+                                            quit = true;
                                         }
-                                    } else {
-                                        quit = true;
                                     }
                                 }
-                            }
 
-                        } catch (ConnectException e) {
-                            System.err.println("Unable to connect to the server. Are you sure the server is up?");
-                        } catch (Exception e) {
-                            throw new RuntimeException(e);
+                            } catch (ConnectException e) {
+                                System.err.println("Unable to connect to the server. Are you sure the server is up?");
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        else{
+                            System.out.println("Please enter a valid ip!");
                         }
 
                     } else if (Objects.equals(message, ":quit")) {
@@ -63,5 +69,21 @@ public class Client {
                 }
             }
         }
+    }
+
+    private static boolean ipValidator(String message){
+        boolean valid = true;
+        String ip = message.split(" ")[1];
+        int port = Integer.parseInt(ip.split(":")[1]);
+        for (int i = 0; i < 4; i++) {
+            int ipPart = Integer.parseInt(ip.split(":")[0].split("\\.")[i]);
+            if (ipPart < 0 || ipPart > 256){
+                valid = false;
+            }
+        }
+        if (port < 0 || port > 65535){
+            valid = false;
+        }
+        return valid;
     }
 }
