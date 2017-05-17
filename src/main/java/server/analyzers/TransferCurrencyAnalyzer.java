7b343b25d2871acf95f5c84deb27efec137de07a
ilysion <hannes.saariste@gmail.com>
2017-05-17 14:47:18 +0300

    Added currency transfer and bugfixes with currency.

---
+++ src/main/java/server/analyzers/TransferCurrencyAnalyzer.java
@@ -0,0 +1,66 @@
+package server.analyzers;
+
+import server.Client;
+import server.Database;
+import server.ServerProperties;
+import server.enums.ResponseType;
+import server.enums.UserStanding;
+
+/**
+ * Created by ilysion on 17.05.2017.
+ */
+public class TransferCurrencyAnalyzer implements CommandAnalyzer{
+
+    private final String keyword = ServerProperties.getKeyword(this.getClass());
+
+    @Override
+    public boolean isValidCommand(String[] splittedUserInput) {
+        return splittedUserInput.length == 3 && keyword.equals(splittedUserInput[0]);
+    }
+
+    @Override
+    public ResponseType analyze(Client client, String[] splittedUserInput) {
+
+        if (Database.getUserStanding(client.getUsername()) == UserStanding.ADMIN) {
+            Double targetAddCurrency;
+            try{
+                targetAddCurrency = Double.parseDouble(splittedUserInput[2]);
+            }
+            catch (Exception e){
+                return ResponseType.INVALID_CURRENCY;
+            }
+            String targetUsernameFrom = client.getUsername();
+            String targetUsernameTo = splittedUserInput[1];
+            if (Database.doesUserExist(targetUsernameFrom )&& Database.doesUserExist(targetUsernameTo)) {
+                Double fromUserCurrency = Database.getUserCurrency(targetUsernameFrom);
+
+                if(fromUserCurrency < targetAddCurrency){
+                    return ResponseType.NOT_ENOUGH_CURRENCY;
+                }
+                if(targetAddCurrency<0){
+                    return ResponseType.INVALID_CURRENCY;
+                }
+                Database.addUserCurrency(targetUsernameFrom, targetAddCurrency * -1);
+                Database.addUserCurrency(targetUsernameTo, targetAddCurrency);
+                Double currency = Database.getUserCurrency(targetUsernameTo );
+                client.addToQueue("Your currency is now: " + currency + " coins." );
+
+                return ResponseType.GENERAL_SUCCESS;
+            }
+            else {
+                return ResponseType.USER_DOES_NOT_EXIST;
+            }
+
+        }
+        else {
+            return ResponseType.NOT_ALLOWED;
+        }
+
+    }
+
+    @Override
+    public String getKeyword() {
+        return keyword;
+    }
+
+}
